12db945dbe863551620c495afabdb68587e091e5
Admin <admin@Admins-MacBook-Pro.local>
2024-12-18 22:43:34 +0530

    created routes with some styles and ui

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${lusitana.variable} font-inter antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lusitana: 'var(--font-lusitana)', // note: you can call the left side of this whatever you want - barlow-bold or title-font or foo-bar, this is what you'll use in your Tailwind css classes to use this font
+        inter: 'var(--font-inter)', // note: the bit that goes inside the var() function is the same variable name we defined in app.tsx
+      },
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
+export default config;
